fd4b1322392010947f673994308bae677e3fa436
Jin Choi <choij@cern.ch>
2021-04-01 14:28:13 +0900

    update AnalysisTools/CandlestickMaker.py

---
+++ AnalysisTools/CandlestickMaker.py
@@ -0,0 +1,117 @@
+import os, shutil
+import warnings
+warnings.filterwarnings(action='ignore')
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from mpl_finance import candlestick2_ochl, volume_overlay
+from datetime import datetime, timedelta
+
+class CandlestickMaker():
+    def __init__(self, sample, start_date, end_date):
+        """ Initialize CandlestickManager """
+        """ This class will generate candlestick images """
+        """ from start_date to end_date """
+        """ Note that the index of the sample is assumed to be datetime.date """
+        first_date = sample.index[0]
+        final_date = sample.index[-1]
+        print(f"==== Initializing CandlestickManager...")
+        print(f"==== Sample Info")
+        print(f"==== first date: {first_date}")
+        print(f"==== final date: {final_date}")
+        print(f"==== CandlestickManager will make candlestick images")
+        print(f"==== from {start_date} to {end_date}\n")
+
+        start_date = datetime.strptime(start_date, '%Y-%m-%d')
+        end_date = datetime.strptime(end_date, '%Y-%m-%d')
+        self.start_date = start_date.date()
+        end_date = end_date.date()
+
+        cushion_date = start_date - timedelta(days=50)
+        cushion_date = cushion_date.date()
+        if cushion_date < first_date:
+            print(f"==== Warning! {cushion_date} before {first_date}")
+            print(f"==== may lost first few candlesticks")
+        self.cushion = sample[cushion_date:end_date]
+
+    def ohlc_to_candlestick(self, coin_name="coin_Bitcoin", days=5, train=True, use_volume=False):
+        # make directories to data folder
+        base_dir = os.getcwd() + "/data/Candlesticks/" + coin_name + "/" + str(days) + "days"
+        if use_volume:
+            base_dir += "/ohlcv"
+        else:
+            base_dir += "/ohlc"
+        if train:
+            base_dir += "/train"
+        else:
+            base_dir += "/test"
+        print(f"==== Creating candlestick images...")
+        print(f"==== Images will be storced in {base_dir}")
+
+        try:
+            os.makedirs(base_dir)
+            os.makedirs(base_dir+"/up")
+            os.makedirs(base_dir+"/down")
+        except:
+            print(f"==== Warning! directory '{base_dir}' exists! Force to overwrite")
+            shutil.rmtree(base_dir)
+            os.makedirs(base_dir)
+            os.makedirs(base_dir+"/up")
+            os.makedirs(base_dir+"/down")
+
+        sample = self.cushion.copy()
+        sample['number'] = sample.index.map(mdates.date2num)
+
+        plt.style.use('dark_background')
+
+        # make labels
+        # Labels are based on the last n days candlesticks, which makes more easy to deal with index
+        # However, it could be confused in learning step
+        # if raise tomorrow -> up, else -> down
+        idx = 0
+        for date in sample.index:
+            if date < self.start_date:
+                idx += 1
+                continue
+            
+            if date != sample.index[idx]:
+                print(f"==== Warning: {date} and {sample.index[idx]} is not equal!")
+            
+            label = ""
+            if sample.loc[date, 'Close'] > sample.loc[date, 'Open']:
+                label = "up"
+            else:
+                label = "down"
+            
+            ohlc = sample.iloc[idx-days:idx, :]
+            idx += 1
+            dimension = 512
+            my_dpi = 96
+            fig = plt.figure(figsize=(dimension/my_dpi, dimension/my_dpi), dpi=my_dpi)
+            ax1 = fig.add_subplot(1, 1, 1)
+            candlestick2_ochl(ax1, ohlc['Open'], ohlc['Close'], ohlc['High'], ohlc['Low'],
+                                width=1, colorup='#77d879', colordown='#db3f3f')
+            ax1.grid(False)
+            ax1.set_xticklabels([])
+            ax1.set_yticklabels([])
+            ax1.xaxis.set_visible(False)
+            ax1.yaxis.set_visible(False)
+            ax1.axis('off')
+           
+            if use_volume:
+                ax2 = ax1.twinx()
+                bc = volume_overlay(ax2, ohlc['Open'], ohlc['Close'], ohlc['Volume'],
+                                    width=1, colorup='#77d879', colordown='#db3f3f', alpha=0.5)
+                ax2.add_collection(bc)
+                ax2.grid(False)
+                ax2.set_xticklabels([])
+                ax2.set_yticklabels([])
+                ax2.xaxis.set_visible(False)
+                ax2.yaxis.set_visible(False)
+                ax2.axis('off')
+            
+            # save figure
+            pngfile = os.path.join(base_dir, label, date.strftime("%Y-%m-%d")+".png")
+            fig.savefig(pngfile, pad_inches=0, transparent=False)
+            plt.close()
+        print("==== Conversion for {coin_name} is finished\n")
---
+++ PrepareCandlestick.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
+from AnalysisTools.CandlestickMaker import CandlestickMaker
+
+df = pd.read_csv("./data/coin_Bitcoin.csv")
+df = df[['Date', 'High', 'Low', 'Open', 'Close', 'Volume']]
+
+# preprocess
+for idx in df.index:
+    if df.loc[idx, 'Volume'] == 0.:
+        df.loc[idx, 'Volume'] = np.nan
+    date_time = df.loc[idx, 'Date'].split(" ")
+    date, time = date_time[0], date_time[1]
+    df.loc[idx, 'Date'] = datetime.strptime(date, '%Y-%m-%d').date()
+df = df.set_index('Date')
+df = df.dropna()
+
+# set daily returns and accumulated returns
+#df['rtn'] = df['Close'].pct_change()
+#df['acc_rtn'] = (1.0 + df['rtn']).cumprod()
+
+# create train dataset
+TrainMaker = CandlestickMaker(df, "2014-06-01", "2019-12-31")
+TrainMaker.ohlc_to_candlestick(days=5, coin_name="coin_Bitcoin", train=True, use_volume=False)
+
+TestMaker = CandlestickMaker(df, "2020-01-01", "2020-12-31")
+TestMaker.ohlc_to_candlestick(days=5, coin_name="coin_Bitcoin", train=False, use_volume=False)

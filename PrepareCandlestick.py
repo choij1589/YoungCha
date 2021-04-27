62a1f87725b26d639f0a47872c2359b85453447d
Jin Choi <choij@cern.ch>
2021-04-27 16:11:41 +0900

    regular update

--- PrepareCandlestick.py
+++ PrepareCandlestick.py
@@ -5,7 +5,11 @@ from AnalysisTools.CandlestickMaker import CandlestickMaker
 
 df = pd.read_csv("./data/coin_Bitcoin.csv")
 df = df[['Date', 'High', 'Low', 'Open', 'Close', 'Volume']]
-df['MA20'] = df['Close'].rolling(window=20).mean()
+df['EMA20'] = df['Close'].ewm(span=20).mean()
+df['EMA40'] = df['Close'].ewm(span=40).mean()
+df['MACD'] = df['EMA40'] - df['EMA20']
+df['signal'] = df['MACD'].ewm(span=10).mean()
+df['MACD-hist'] = df['MACD'] - df['signal']
 
 # preprocess
 for idx in df.index:
@@ -23,11 +27,13 @@ df = df.dropna()
 
 # train dataset
 TrainMaker = CandlestickMaker(df, "2015-01-01", "2019-12-31")
-TrainMaker.ohlc_to_candlestick(days=20, coin_name="coin_Bitcoin", train=True, use_volume=False)
+TrainMaker.ohlc_to_candlestick(
+    days=20, coin_name="coin_Bitcoin", train=True, use_volume=False)
 
 # test dataset
 TestMaker = CandlestickMaker(df, "2020-01-01", "2020-12-31")
-TestMaker.ohlc_to_candlestick(days=20, coin_name="coin_Bitcoin", train=False, use_volume=False)
+TestMaker.ohlc_to_candlestick(
+    days=20, coin_name="coin_Bitcoin", train=False, use_volume=False)
 
 #maker = CandlestickMaker(df, "2020-01-01", "2020-02-01")
-#maker.ohlc_to_candlestick(days=10, coin_name="coin_Bitcoin", train=True, use_volume=True)
+#maker.ohlc_to_candlestick(days=10, coin_name="coin_Bitcoin", train=True, use_volume=False)
